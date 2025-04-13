ff98348f3067a61009f3d1cf0bc4560d4c702e5d
Logan Wilkerson <lcwilker@gmail.com>
2025-04-13 15:26:16 -0400

    playing with collisions

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -1,1 +1,6 @@
 extends Node
+
+
+var collision_layer_environment: int = 1
+var collision_layer_player: int = 2
+var collision_layer_enemy: int = 3
--- npcs/target_dummy.gd
+++ npcs/target_dummy.gd
@@ -4,4 +4,9 @@ extends Entity
 func _physics_process(delta: float) -> void:
 	super(delta)
 	$Marker2D/Label.text = str(current_health)
-	move_and_slide()
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+		current_knockback = current_knockback.bounce(collision.get_normal())
+		#velocity = velocity.slide(collision.get_normal())
+		#current_knockback = current_knockback.slide(collision.get_normal())
